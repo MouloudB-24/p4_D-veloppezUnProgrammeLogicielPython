f3fd00eff962d2d143219f1c299d7b33f9004ab4
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-27 14:47:05 +0200

    Tournament class update

--- models/tournament.py
+++ models/tournament.py
@@ -1,41 +1,40 @@
 class Tournament:
-    def __init__(self, name, location, start_date, end_date, rounds=4, current_round=1, description=""):
+    def __init__(self, name, location, start_date, end_date, number_of_rounds=4, current_round=1, description=""):
         self.name = name
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
-        self.rounds = rounds
+        self.number_of_rounds = number_of_rounds
         self.current_round = current_round
-        self.matches = [[] for _ in range(rounds)]
+        self.rounds = [[] for _ in range(number_of_rounds)]
         self.registered_players = []
         self.description = description
 
     def add_player(self, player):
         self.registered_players.append(player)
 
-    def add_match(self, match):
-        self.matches[self.current_round - 1].append(match)
-
-    def award_match_points(self, match_index, winner_index):
-        match = self.matches[self.current_round - 1][match_index]
-        winner, loser = match
-        if winner_index == 0:
-            winner.add_points()
-        elif winner_index == 1:
-            loser.add_points()
-        else:
-            winner.add_points(0.5)
-            loser.add_points(0.5)
+    def add_round(self, round):
+        self.rounds[self.current_round - 1].append(round)
 
     def start_next_round(self):
-        if self.current_round < self.rounds:
+        if self.current_round < self.number_of_rounds:
             self.current_round += 1
-            self.matches[self.current_round - 1] = []
+            self.rounds[self.current_round - 1] = []
+
+    def show_tournament_info(self):
+        return {
+            "name": self.name,
+            "location": self.location,
+            "date": f"{self.start_date} - {self.end_date}",
+            "rounds": self.rounds,
+            "current_round": self.current_round,
+            "description": self.description
+        }
 
     def __str__(self):
-        return f"{self.name} {self.registered_players})"
+        return f"{self.show_tournament_info()})"
 
 
 if __name__ == "__main__":
-    tournament = Tournament("Tournoi d'échecs", "Paris", "30/09/2024", "15/10/2024", rounds=4)
+    tournament = Tournament("Tournoi d'échecs", "Paris", "30/09/2024", "15/10/2024", number_of_rounds=4)
     print(tournament.registered_players)
