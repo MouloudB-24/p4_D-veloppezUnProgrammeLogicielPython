11e30c072eeec53190903ddb5b83f08b71557124
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-28 16:30:56 +0200

    Match class update

--- models/match.py
+++ models/match.py
@@ -2,25 +2,34 @@ import random
 
 
 class Match:
-    def __init__(self, player_1, player_2, score_1=0, score_2=0):
+    def __init__(self, player_1, player_2):
         self.player_1 = player_1
-        self.score_1 = score_1
         self.player_2 = player_2
-        self.score_2 = score_2
-        self.match = ([player_1, score_1], [player_2, score_2])
+        self.score_1 = 0
+        self.score_2 = 0
 
     def award_match_points(self):
-        winner_index = random.randint(0, 1)
+        winner_index = random.randint(0, 2)
         if winner_index == 0:
             self.score_1 += 1
+            self.player_1.add_point(1)
         elif winner_index == 1:
             self.score_2 += 1
+            self.player_2.add_point(1)
         else:
             self.score_1 += 0.5
             self.score_2 += 0.5
+            self.player_1.add_point(0.5)
+            self.player_1.add_point(0.5)
 
-        self.match[0][1] = self.score_1
-        self.match[1][1] = self.score_2
+    def get_match_score(self):
+        return [self.player_1.first_name, self.score_1], [self.player_2.first_name, self.score_2]
 
     def __repr__(self):
-        return f"{self.match}"
+        return f"{self.get_match_score()}"
+
+
+if __name__ == "__main__":
+    match0 = Match("Aylan", "Mouloud")
+    match0.award_match_points()
+    print(match0)
