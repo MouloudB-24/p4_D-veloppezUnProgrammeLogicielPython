45ab62f60139b9b75ebcb0f88ed320f4ddaf9d22
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-28 16:32:13 +0200

    Tournament class update

--- models/tournament.py
+++ models/tournament.py
@@ -1,5 +1,5 @@
 class Tournament:
-    def __init__(self, name, location, start_date, end_date, number_of_rounds=4, current_round=1, description=""):
+    def __init__(self, name, location, start_date, end_date, description="", number_of_rounds=4, current_round=1):
         self.name = name
         self.location = location
         self.start_date = start_date
@@ -10,29 +10,30 @@ class Tournament:
         self.registered_players = []
         self.description = description
 
-    def add_player(self, player):
-        self.registered_players.append(player)
+    def register_player(self, player):
+        self.registered_players.append(player.get_player_info())
 
     def add_round(self, round):
-        self.rounds[self.current_round - 1].append(round)
+        self.rounds[self.current_round - 1].append(round.matches)
 
     def start_next_round(self):
         if self.current_round < self.number_of_rounds:
             self.current_round += 1
             self.rounds[self.current_round - 1] = []
 
-    def show_tournament_info(self):
+    def get_tournament_info(self):
         return {
             "name": self.name,
             "location": self.location,
             "date": f"{self.start_date} - {self.end_date}",
+            "list_of_players": self.registered_players,
             "rounds": self.rounds,
             "current_round": self.current_round,
             "description": self.description
         }
 
     def __str__(self):
-        return f"{self.show_tournament_info()})"
+        return f"{self.get_tournament_info()})"
 
 
 if __name__ == "__main__":
