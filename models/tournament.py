fef31c863610bf30265cbe6c161458fe039354d4
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-30 19:17:33 +0200

    Adding set methods

--- models/tournament.py
+++ models/tournament.py
@@ -1,23 +1,39 @@
 class Tournament:
-    def __init__(self, name, location, start_date, end_date, description="", number_of_rounds=4, current_round=1):
+    def __init__(self):
+        self.name = None
+        self.location = None
+        self.start_date = None
+        self.end_date = None
+        self.description = None
+        self.number_rounds = 4
+        self.current_round = 1
+        self.rounds = [[] for _ in range(self.number_rounds)]
+        self.registered_players = []
+
+    def set_name(self, name):
         self.name = name
+
+    def set_location(self, location):
         self.location = location
+
+    def set_date(self, start_date, end_date):
         self.start_date = start_date
         self.end_date = end_date
-        self.number_of_rounds = number_of_rounds
-        self.current_round = current_round
-        self.rounds = [[] for _ in range(number_of_rounds)]
-        self.registered_players = []
+
+    def set_number_rounds(self, number_rounds):
+        self.number_rounds = number_rounds
+
+    def set_description(self, description):
         self.description = description
 
     def register_player(self, player):
-        self.registered_players.append(player.get_player_info())
+        self.registered_players.append(player.get_player())
 
     def add_round(self, round):
         self.rounds[self.current_round - 1].append(round.matches)
 
     def start_next_round(self):
-        if self.current_round < self.number_of_rounds:
+        if self.current_round < self.number_rounds:
             self.current_round += 1
             self.rounds[self.current_round - 1] = []
 
@@ -37,5 +53,6 @@ class Tournament:
 
 
 if __name__ == "__main__":
-    tournament = Tournament("Tournoi d'échecs", "Paris", "30/09/2024", "15/10/2024", number_of_rounds=4)
-    print(tournament.registered_players)
+    #tournament = Tournament("Tournoi d'échecs", "Paris", "30/09/2024", "15/10/2024", number_of_rounds=4)
+    #print(tournament.registered_players)
+    pass
