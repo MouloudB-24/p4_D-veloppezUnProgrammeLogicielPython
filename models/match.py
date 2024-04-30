097bbb7251ca21e23a8a7c34595840ca0e8515d5
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-30 19:46:01 +0200

    Update __init__ method

--- models/match.py
+++ models/match.py
@@ -2,13 +2,17 @@ import random
 
 
 class Match:
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    def __init__(self):
+        self.player_1 = None
+        self.player_2 = None
         self.score_1 = 0
         self.score_2 = 0
 
-    def award_match_points(self):
+    def set_players(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def award_points(self):
         winner_index = random.randint(0, 2)
         if winner_index == 0:
             self.score_1 += 1
@@ -20,16 +24,17 @@ class Match:
             self.score_1 += 0.5
             self.score_2 += 0.5
             self.player_1.add_point(0.5)
-            self.player_1.add_point(0.5)
+            self.player_2.add_point(0.5)
 
-    def get_match_score(self):
+    def get_score(self):
         return [self.player_1.first_name, self.score_1], [self.player_2.first_name, self.score_2]
 
     def __repr__(self):
-        return f"{self.get_match_score()}"
+        return f"{self.get_score()}"
 
 
 if __name__ == "__main__":
-    match0 = Match("Aylan", "Mouloud")
-    match0.award_match_points()
-    print(match0)
+    match = Match()
+    match.set_players("Aylan", "Mouloud")
+    match.award_points()
+    print(match)
