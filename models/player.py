f0c2093e2f21dc7c7eb10a018b037a2042469c29
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-04-30 16:38:59 +0200

    adding save and set methods

--- models/player.py
+++ models/player.py
@@ -1,16 +1,37 @@
+import json
+from pathlib import Path
+
+
 class Player:
-    def __init__(self, first_name, last_name, date_of_birth, chess_id, sex=None):
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+        self.date_of_birth = None
+        self.sex = None
+        self.chess_id = None
+        self.points = 0
+        self.save_folder = Path.cwd().parent / "data" / "tournament"
+
+    def set_name(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.date_of_birth = date_of_birth
-        self.chess_id = chess_id
-        self.sex = sex
-        self.points = 0
 
-    def add_point(self, score):
+    def set_date_of_birth(self, date_of_birth):
+        self.date_of_birth = date_of_birth
+
+    def set_sex(self, sex):
+        self.sex = sex
+
+    def set_chess_id(self, chess_id):
+        self.chess_id = chess_id
+
+    def set_save_folder(self, save_folder):
+        self.save_folder = save_folder
+
+    def update_points(self, score):
         self.points += score
 
-    def get_player_info(self):
+    def get_player(self):
         return {
             "first_name": self.first_name,
             "last_name": self.last_name,
@@ -20,15 +41,41 @@ class Player:
             "points": self.points
         }
 
-    def update_player_info(self, **kwargs):
+    def update_player(self, **kwargs):
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
 
-    def __str__(self):
-        return f"{self.get_player_info()}"
+    def save_player(self):
+        self.save_folder.mkdir(parents=True, exist_ok=True)
+        save_file = self.save_folder / "players.json"
+
+        # Load the existing player list if there is one
+        if save_file.exists():
+            with open(save_file, "r") as file:
+                players_list = json.load(file)
+        else:
+            players_list = []
+
+        new_player = self.get_player()
+        registered_player = any(new_player["chess_id"] == player["chess_id"] for player in players_list)
+
+        # Add new player
+        if not registered_player:
+            players_list.append(new_player)
+
+            # Save updated player list
+            with open(save_file, "w") as file:
+                json.dump(players_list, file, indent=2)
+        else:
+            print(f"The player {new_player} already existed in the database")
 
 
 if __name__ == "__main__":
-    aylan = Player("Aylan", "BELLIL", "30/07/2023", "AB98762", "M")
-    print(aylan)
+    aylan = Player()
+    aylan.set_name("Aylan", "BELLIL")
+    aylan.set_date_of_birth( "30/07/2023")
+    aylan.set_sex("M")
+    aylan.set_chess_id("AB98762")
+    aylan.save_player()
+
