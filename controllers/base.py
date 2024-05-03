ac7f67bfcb9e33986daa2c094c94712eed7d54be
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-05-03 11:30:41 +0200

    add shuffle_players method

--- controllers/base.py
+++ controllers/base.py
@@ -1,2 +1,9 @@
+import random
 
 
+class Controller:
+    def __init__(self, tournament):
+        self.tournament = tournament
+
+    def shuffle_players(self):
+        return random.shuffle(self.tournament.registered_players)
